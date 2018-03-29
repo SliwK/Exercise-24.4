5ba1d1b9a1ec92112eafa60f1db6b9787fe62041
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-03-29 23:26:34 +0200

    Add gitignore

---
+++ index.js
@@ -0,0 +1,26 @@
+const formatDate = (timeInSeconds) => {
+
+const hour = Math.floor(timeInSeconds/3600);
+const restHour = timeInSeconds%3600;
+const min = Math.floor(restHour/60);
+const sec = Math.round(restHour%60);
+
+
+if(timeInSeconds == null) {
+  return '0s';
+} else if(hour === 0 && min ===0) {
+  return sec+'s';
+} else if(hour === 0 && sec ===0){
+  return min+'m';
+} else if(min === 0 && sec === 0){
+  return hour+'h';
+} else if(hour === 0) {
+  return min+'m ' + sec+'s';
+} else if(min === 0) {
+  return hour+'h ' + sec+'s';
+} else if(sec === 0){
+  return hour+'h '+ min+'m';
+} else {return hour+'h ' + min+'m ' + sec+'s';}
+};
+
+module.exports = formatDate;
